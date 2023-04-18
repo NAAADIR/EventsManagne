d50ce7b556f9ab9fae5da88ad7a2ac22c1da23d5
NAAADIR <nadir.boumazaa@gmail.com>
2023-04-18 15:22:47 +0200

    Add: Participants to Evenements

--- src/app/visiteur/visiteur.component.ts
+++ src/app/visiteur/visiteur.component.ts
@@ -37,32 +37,57 @@ export class VisiteurComponent {
     ).valueChanges();
   }
 
-  participer(evenement: any) {
-    if (!evenement) {
-      console.error('Evenement est null ou undefined');
+  participer(typeEvenement: string) {
+    // Vérifier si l'utilisateur est connecté
+    if (!this.authService.isAuthenticated()) {
+      console.error("L'utilisateur n'est pas connecté");
       return;
     }
-    this.authService.getToken().subscribe(token => {
-      if (token.isValid()) {
-        // Ajouter l'UID de l'utilisateur à la liste des participants
-        const currentUserUid = token.getPayload().sub;
-        evenement.participants.push(currentUserUid);
-
-        // Mettre à jour le nombre de participants dans Firebase
-        const evenementDoc = this.afs.collection('evenements').doc(evenement.id);
-        evenementDoc.update({
-          participants: evenement.participants,
-          nombreParticipants: evenement.participants.length
-        }).then(() => {
-          console.log('Le visiteur a été ajouté à la liste des participants.');
-        }).catch(error => {
-          console.error('Une erreur est survenue lors de la mise à jour des participants :', error);
-        });
-      } else {
-        console.error('Le token d\'authentification n\'est pas valide.');
+  
+    this.authService.getToken().subscribe((token) => {
+      const userId = token.getPayload().sub;
+      if (!userId) {
+        console.error("Impossible de récupérer l'identifiant de l'utilisateur connecté");
+        return;
       }
+  
+      // Récupérer le document contenant l'événement avec le typeEvenement donné
+      this.afs.collection<Evenement>('evenements', ref =>
+        ref.where('typeEvenement', '==', typeEvenement)
+      ).get().subscribe((querySnapshot) => {
+        // Vérifier qu'un document a été trouvé
+        if (querySnapshot.docs.length === 0) {
+          console.warn(`Aucun événement trouvé pour le type ${typeEvenement}`);
+          return;
+        }
+  
+        // Mettre à jour le premier document trouvé avec le nouvel utilisateur
+        const evenement = querySnapshot.docs[0].data() as Evenement;
+        if (!evenement.participants) {
+          console.warn("La liste des participants n'a pas été initialisée");
+          evenement.participants = [];
+        }
+        if (evenement.participants.includes(userId)) {
+          console.warn("L'utilisateur est déjà inscrit à cet événement");
+          return;
+        }
+        evenement.participants.push(userId);
+        evenement.nombreParticipants = evenement.participants.length;
+  
+        // Mettre à jour le document dans Firestore
+        this.afs.collection<Evenement>('evenements').doc(querySnapshot.docs[0].id).update(evenement)
+          .then(() => {
+            console.log("L'événement a été mis à jour avec succès");
+          })
+          .catch((error) => {
+            console.error("Une erreur s'est produite lors de la mise à jour de l'événement :", error);
+          });
+      });
     });
   }
+  
+  
+  
 
 }
 
